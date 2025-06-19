a39fc55ccafbae0aa732c27ee3857a5c9e725f04
Andriy <grushevoyandrey@gmail.com>
2025-06-19 17:40:11 +0100

    Add rest_framework and simplejwt to installed apps

--- DRF_Library/settings.py
+++ DRF_Library/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework_simplejwt",
+    "rest_framework",
     "debug_toolbar",
 ]
 
