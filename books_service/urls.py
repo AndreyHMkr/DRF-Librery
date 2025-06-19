a4969ca9ec1c7491822a19b81c8661ed32550eee
Andriy <grushevoyandrey@gmail.com>
2025-06-19 19:18:09 +0100

    cleaned urls.py

--- books_service/urls.py
+++ books_service/urls.py
@@ -3,6 +3,4 @@ from rest_framework.routers import DefaultRouter
 from books_service.views import BookViewSet
 
 app_name = "books_service"
-router = DefaultRouter()
-router.register("books", BookViewSet, basename="books")
-urlpatterns = router.urls
+urlpatterns = []
