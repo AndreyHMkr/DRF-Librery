59383c082d7619f264254ef2e935b122f5ce0112
Andriy <grushevoyandrey@gmail.com>
2025-06-19 14:21:21 +0100

    add debug_toolbar

--- DRF_Library/settings.py
+++ DRF_Library/settings.py
@@ -36,10 +36,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -48,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "DRF_Library_practice.urls"
+ROOT_URLCONF = "DRF_Library.urls"
 
 TEMPLATES = [
     {
@@ -119,3 +121,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
