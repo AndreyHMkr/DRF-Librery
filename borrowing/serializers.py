0aea3ea342d7ac97ea2754963e12b0b95728e1b7
Andriy <grushevoyandrey@gmail.com>
2025-06-20 14:50:20 +0100

    "Split BorrowingSerializer into List & Retrieve"

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,9 +1,51 @@
 from rest_framework import serializers
 
+from books_service.models import Book
+from books_service.serializers import BookSerializer
 from borrowing.models import Borrowing
+from user.serializers import UserSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
+    book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all())
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrowing_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
+        read_only_fields = ("id", "user")
+
+
+class BorrowingListSerializer(serializers.ModelSerializer):
+    book = serializers.SlugRelatedField(read_only=True, slug_field="title")
+    user = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    is_returned = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrowing_date",
+            "expected_return_date",
+            "book",
+            "user",
+            "is_returned",
+        )
+
+    def get_is_returned(self, obj):
+        return obj.actual_return_date is not None
+
+
+class BorrowingRetrieveSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    book = BookSerializer(read_only=True)
+
     class Meta:
         model = Borrowing
         fields = (
