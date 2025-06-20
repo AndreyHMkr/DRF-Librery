2c449e5d16b5dcb891edcb1ebd285811fba7cc16
Andriy <grushevoyandrey@gmail.com>
2025-06-20 14:58:46 +0100

    Add return_book action to handle book return and inventory update

--- borrowing/views.py
+++ borrowing/views.py
@@ -7,6 +7,9 @@ from borrowing.serializers import BorrowingSerializer
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnlyBorrowing]
+
     def perform_create(self, serializer):
         book = serializer.validated_data["book"]
         if book.inventory <= 0:
@@ -16,3 +19,18 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         book.inventory -= 1
         book.save()
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
+    def return_book(self, request, pk=None):
+        borrowing = self.get_object()
+        if borrowing.actual_return_date:
+            raise serializers.ValidationError(
+                "This borrowing has already been returned."
+            )
+
+        borrowing.actual_return_date = now().date()
+        borrowing.book.inventory += 1
+        borrowing.book.save()
+        borrowing.save()
+        return Response({"status": "book returned"})
+
