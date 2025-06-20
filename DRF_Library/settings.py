454d8643858ec63802ea6c041c37b0545a9baf80
Andriy <grushevoyandrey@gmail.com>
2025-06-20 18:32:44 +0100

    Add dotenv support to settings

--- DRF_Library/settings.py
+++ DRF_Library/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,7 +44,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "user",
     "books_service",
-    "borrowing",
+    "borrowing.apps.BorrowingConfig",
 ]
 
 MIDDLEWARE = [
