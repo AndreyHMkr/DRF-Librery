51046a1cffe616ccfde7496ab8275a4909adb511
Andriy <grushevoyandrey@gmail.com>
2025-06-20 12:03:01 +0100

    feat(user): add 'me/' endpoint to user URLs

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from user.views import CreateUserView, CreateTokenView, LogoutView
+from user.views import CreateUserView, CreateTokenView, LogoutView, UserMeView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("token/", CreateTokenView.as_view(), name="token"),
+    path("me/", UserMeView.as_view(), name="me"),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
