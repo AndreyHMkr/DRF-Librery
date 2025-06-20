afdbf0705a24295f5d1a53a09cda268d92a6eb26
Andriy <grushevoyandrey@gmail.com>
2025-06-20 14:57:26 +0100

    Add perform_create method to handle book inventory check and update

--- borrowing/views.py
+++ borrowing/views.py
@@ -7,3 +7,12 @@ from borrowing.serializers import BorrowingSerializer
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    def perform_create(self, serializer):
+        book = serializer.validated_data["book"]
+        if book.inventory <= 0:
+            raise serializers.ValidationError(
+                "This book is out of stock. Please try again later."
+            )
+        book.inventory -= 1
+        book.save()
+        serializer.save(user=self.request.user)
