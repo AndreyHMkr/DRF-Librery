aa5a6586d434bf6c66f35944f1232a82230be94b
Andriy <grushevoyandrey@gmail.com>
2025-06-21 19:47:40 +0100

    add PaymentSerializer class to serializers.py

---
+++ payments/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from payments.models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "status",
+            "type",
+            "borrowing_id",
+            "session_url",
+            "session_id",
+            "created_at",
+        )
+        read_only_fields = ("id", "created_at", "session_id", "session_url")
