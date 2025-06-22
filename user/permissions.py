94b13c71db15c87e49fe6a304d1943ad8f02be77
Andriy <grushevoyandrey@gmail.com>
2025-06-22 14:04:01 +0100

    Update custom permission: Admin has oll actions, customer borrowing create & book list

--- user/permissions.py
+++ user/permissions.py
@@ -1,11 +1,8 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAdminOrReadOnlyBorrowing(BasePermission):
+class IsAdminOllActionOllReadOnlyAndBorrowingAction(BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
-
-    def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        return request.user.is_staff
+        return request.user and request.user.is_staff
